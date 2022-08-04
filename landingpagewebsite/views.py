6c029ea273533a1df6499b4c8f2309b12a4d8909
Konstantin <midvo55@gmail.com>
2022-08-04 10:08:12 +0300

    second_commit

---
+++ landingpagewebsite/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def first_page(request):
+    a = '<h1>Hello World</h1>'
+    text = 'Новый текст'
+    return render(request, './index.html', {
+        'a': a,
+        'text': text
+    }
+    )
