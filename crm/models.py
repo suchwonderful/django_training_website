e856f9967a4838f72f7eda4df98f0ea4e9b2cbb1
Konstantin <midvo55@gmail.com>
2022-10-12 00:14:08 +0300

    third commit

--- crm/models.py
+++ crm/models.py
@@ -24,3 +24,14 @@ class Order(models.Model):
         verbose_name = 'Заказ'
         verbose_name_plural = 'Заказы'
 
+class CommentCrm(models.Model):
+    comment_binding = models.ForeignKey(Order, on_delete=models.CASCADE, verbose_name='Заявка')
+    comment_text = models.TextField(verbose_name = 'Текст комментария')
+    comment_dt = models.DateTimeField(auto_now=True, verbose_name='Дата создания')
+
+    def __str__(self):
+        return self.comment_text
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Коментарии'
